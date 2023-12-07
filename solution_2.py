af81850b62b7bf360e63244ce72a60cbcd18f62f
Alleg-P <144456205+Alleg-P@users.noreply.github.com>
2023-12-07 12:35:52 +0300

    solution_2 file has been fixed

---
+++ solution_2.py
@@ -0,0 +1,6 @@
+workers = ["Света", "Олег", "Маша", "Паша"]
+
+even_days = workers[1::2]
+odd_days = workers[::2]
+
+print(f'В нечётные дни работают: {odd_days}\n\nВ чётные дни работают: {even_days}')
